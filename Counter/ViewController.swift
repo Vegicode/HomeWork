73d9f846fcc296eef94e2e8b6e61860cade315bf
Vegiart <egiazarovvadim@yandex.ru>
2024-05-02 11:06:42 +0300

    Внес исправления по рекомендации

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,32 +1,27 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by Mac on 26.04.2024.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
     
-    private var Num: Int = 0
+    private var num: Int = 0
     
-    @IBOutlet weak var Counter: UILabel!
-    @IBOutlet weak var Button: UIButton!
+    @IBOutlet private weak var Counter: UILabel!
+    @IBOutlet private weak var Button: UIButton!
     override func viewDidLoad() {
         
         
         super.viewDidLoad()
-        // Do any additional setup after loading
-        Counter.text = String(Num)
+        
+        Counter.text = String(num)
         
     }
     
 
-    @IBAction func ButtonDidTap(_ sender: Any) {
-        Num += 1
-        Counter.text = String(Num)
+    @IBAction private func ButtonDidTap(_ sender: Any) {
+        num += 1
+        Counter.text = String(num)
     }
     
     
